06aa955e806fc7034f83c88b2447742e0ef377cd
asterasoft <astera2.muhamad@gmail.com>
2018-09-01 14:10:53 +0300

    after change

--- dashboard/posts.js
+++ dashboard/posts.js
@@ -1,115 +1,144 @@
 $(function() {
-    // GET/READ
-    // $('#sub-button').on('click', );
-
-    const loadDatap = function() {
-        $.ajax({
-            url: 'http://localhost:3000/posts',
-            contentType: 'application/json',
-            success: function(response) {
-                var tbodyEl = $('tbody');
-
-                tbodyEl.html('');
-
-                response.forEach(function(post) {
-                    tbodyEl.append('\
-                        <tr>\
-                            <td class="id">' + post.id + '</td>\
-                            <td><input type="text" class="userId" value="' + post.userId + '"></td>\
-                            <td><input type="text" class="categoryId" value="' + post.categoryId + '"></td>\
-                            <td><input type="text" class="title" value="' + post.title + '"></td>\
-                            <td><input type="text" class="body" value="' + post.body + '"></td>\
-                            <td>\
-                            <button class="update-button btn btn-outline-info">Edit</button>\
-                            <button class="delete-button  btn btn-outline-danger"">Delete</i></button>\
-                            </td>\
-                        </tr>\
-                    ');
-                });
-            }
-        });
-    };
-
-    // CREATE/POST
-    $('#create-form').on('submit', function(event) {
-        event.preventDefault();
-
-        let titleInput = $('#title');
-        let DesInput = $('#Describtion');
-        let UID = $('#UID');
-        let CID = $('#CID');
-        
-        $.ajax({
-            url: 'http://localhost:3000/posts',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
-            userId: UID.val(),
-            categoryId: CID.val(),
-            title: titleInput.val(),
-            body:   DesInput.val()
+    // GET method
+   const url= 'http://localhost:3000/posts';
+   
+   function row (post)
+   {
+              var tbodyEl = $('tbody');
+   
+               //tbodyEl.html('');
+               console.log(post)
+               tbodyEl.append('\
+                           <tr>\
+                               <td class="id">' + post.id + '</td>\
+                               <td><input type="text" class="userId" value="' + post.userId + '"></td>\
+                               <td><input type="text" class="categoryId" value="' + post.categoryId + '"></td>\
+                               <td><input type="text" class="title" value="' + post.title + '"></td>\
+                               <td><input type="text" class="body" value="' + post.body + '"></td>\
+                               <td>\
+                               <button class="update-button btn btn-outline-info">Edit</button>\
+                               <button class="delete-button  btn btn-outline-danger"">Delete</i></button>\
+                               </td>\
+                           </tr>\
+                       ');
+                   
+               
+               fetch(url)
+               .then(res=>res.json())
+               .then((data)=>{
+                   data.forEach(function(el) {
+                  row(post);
+                   });
+               });
+           }
+   
+   // POST
+       
+   
+           let titleInput = $('#title');
+           let DesInput = $('#Describtion');
+           let UID = $('#UID');
+           let CID = $('#CID');
+           
+   
+       $("#sub-button").on("click", () => {
+   
+            $.ajax({
+               url: 'http://localhost:3000/posts',
+               method: 'POST',
+               contentType: 'application/json',
+               data: JSON.stringify({
+               userId: UID.val(),
+               categoryId: CID.val(),
+               title: titleInput.val(),
+               body:   DesInput.val()
+             
+           }),
+               
+               success: function(response) {
+                   console.log(response);
+                   UID.val('');
+                   CID.val('');
+                  titleInput.val('');
+                   DesInput.val('');
+                   
+   
+                  
+               }
+           });
+       })
+       //Delete
+       $('table').on('click', '.delete-button', function() {
+           var rowEl = $(this).closest('tr');
+           var id = rowEl.find('.id').text();
+           console.log(rowEl.html);
+   
+           $("#sub-button").on("click", () => {
+   
+           $.ajax({
+               url: 'http://localhost:3000/users/'+ id,
+               method: 'DELETE',
+               contentType: 'application/json',
+               success: function(response) {
+                   console.log(response[0]);
+                  $('#sub-button').click();
+               }
+           });
+           
+       });
+      
+       });
+   
+   
+   //       // UPDATE/PUT
+         $('table').on('click', '.update-button', function() {
+           var rowEl = $(this).closest('tr');
+           var id = rowEl.find('.id').text();
+           var newUser = rowEl.find('.userId').val();
+           var newCate = rowEl.find('.categoryId').val();
+           var newTitle = rowEl.find('.title').val();
+           var newBody = rowEl.find('.body').val();
           
-        }),
-            
-            success: function(response) {
-                console.log(response);
-                UID.val('');
-                CID.val('');
-               titleInput.val('');
-                DesInput.val('');
+           console.log(newUser);
+           console.log(newCate);
+           console.log(newTitle); 
+           console.log(newBody); 
+          
+   
+           $.ajax({
+               url:   'http://localhost:3000/posts/'+ id,
+               method: 'PUT',
+               contentType: 'application/json',
+               data: JSON.stringify({ title: newTitle, body: newBody, id: id, userId:newUser, categoryId:newCate  }),
+               success: function(response) {
+                  // loadDatap();
+                    $('#sub-button').click();
+               }
+           });
+       });
+   
+   //      // DELETE
+       $('table').on('click', '.delete-button', function() {
+           var rowEl = $(this).closest('tr');
+           var id = rowEl.find('.id').text();
+           console.log(rowEl.html);
+           $.ajax({
+               url: 'http://localhost:3000/posts/'+ id,
+               method: 'DELETE',
+               contentType: 'application/json',
+               success: function(response) {
+                   console.log(response[0]);
+                    $('#sub-button').click();
+                  
+               }
+           });
+       });
+   
+   });
+   
                 
-
-                loadDatap();
-                // $('#sub-button').click();
-            }
-        });
-    });
-
-      // UPDATE/PUT
-      $('table').on('click', '.update-button', function() {
-        var rowEl = $(this).closest('tr');
-        var id = rowEl.find('.id').text();
-        var newUser = rowEl.find('.userId').val();
-        var newCate = rowEl.find('.categoryId').val();
-        var newTitle = rowEl.find('.title').val();
-        var newBody = rowEl.find('.body').val();
-       
-        // console.log(newUser);
-        // console.log(newCate);
-        // console.log(newTitle); 
-        // console.log(newBody); 
-       
-
-        $.ajax({
-            url:   'http://localhost:3000/posts/'+ id,
-            method: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify({ title: newTitle, body: newBody, id: id, userId:newUser, categoryId:newCate  }),
-            success: function(response) {
-                loadDatap();
-                // $('#sub-button').click();
-            }
-        });
-    });
-
-     // DELETE
-    $('table').on('click', '.delete-button', function() {
-        var rowEl = $(this).closest('tr');
-        var id = rowEl.find('.id').text();
-        console.log(rowEl.html);
-        $.ajax({
-            url: 'http://localhost:3000/posts/'+ id,
-            method: 'DELETE',
-            contentType: 'application/json',
-            success: function(response) {
-                console.log(response[0]);
-                // $('#sub-button').click();
-                loadDatap();
-            }
-        });
-    });
-   // $('#sub-button').click();
-   loadDatap();
-})
-
-
+   
+   
+   
+   
+   
--- dashboard/users.js
+++ dashboard/users.js
@@ -1,96 +1,102 @@
-$(function() {
-    // GET/READ
-    // $('#sub-button').on('click', );
-
-    const loadData = function() {
-        $.ajax({
-            url: 'http://localhost:3000/users',
-            contentType: 'application/json',
-            success: function(response) {
-                var tbodyEl = $('tbody');
-
-                tbodyEl.html('');
-
-                response.forEach(function(el) {
-                    tbodyEl.append('\
-                        <tr>\
-                            <td class="id">' + el.id + '</td>\
-                            <td><input type="text" class="name" value="' + el.name + '"></td>\
-                            <td><input type="text" class="email" value="' + el.email + '"></td>\
-                            <td>\
-                            <button class="update-button btn btn-outline-info">Edit</button>\
-                            <button class="delete-button btn btn-outline-danger"">Delete</i></button>\
-                            </td>\
-                        </tr>\
-                    ');
-                });
-            }
-        });
-    };
-
-    // CREATE/POST
-    $('.form-inline').on('submit', function(event) {
-        event.preventDefault();
-
-        let userInput = $('#username');
-        let EmailInput = $('#Email');
-        
-        $.ajax({
-            url: 'http://localhost:3000/users',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
-            name: userInput.val(),
-            email: EmailInput.val()}),
-            success: function(response) {
-                console.log(response);
-
-                userInput.val('');
-                EmailInput.val('');
-                loadData();
-                // $('#sub-button').click();
-            }
-        });
-    });
-
-      // UPDATE/PUT
-      $('table').on('click', '.update-button', function() {
-        var rowEl = $(this).closest('tr');
-        var id = rowEl.find('.id').text();
-        var newTitle = rowEl.find('.name').val();
-        var newBody = rowEl.find('.email').val();
-        console.log(newTitle); 
-        console.log(newBody); 
-
-        $.ajax({
-            url:   'http://localhost:3000/users/'+ id,
-            method: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify({ name: newTitle, email: newBody  }),
-            success: function(response) {
-                loadData();
-                // $('#sub-button').click();
-            }
-        });
-    });
-
-     // DELETE
-    $('table').on('click', '.delete-button', function() {
-        var rowEl = $(this).closest('tr');
-        var id = rowEl.find('.id').text();
-        console.log(rowEl.html);
-        $.ajax({
-            url: 'http://localhost:3000/users/'+ id,
-            method: 'DELETE',
-            contentType: 'application/json',
-            success: function(response) {
-                console.log(response[0]);
-                // $('#sub-button').click();
-                loadData();
-            }
-        });
-    });
-   // $('#sub-button').click();
-   loadData();
-})
-
+$(function(){
+    //  Get 
+     const url= 'http://localhost:3000/users';
+          function row (el)
+         {
+                    var tbodyEl = $('tbody');
+ 
+                     //tbodyEl.html('');
+                     console.log(el)
+                     tbodyEl.append('\
+                         <tr>\
+                             <td class="id">' + el.id + '</td>\
+                             <td><input type="text" class="name" value="' + el.name + '"></td>\
+                             <td><input type="text" class="email" value="' + el.email + '"></td>\
+                             <td>\
+                             <button class="update-button btn btn-outline-info">Edit</button>\
+                             <button class="delete-button btn btn-outline-danger"">Delete</i></button>\
+                             </td>\
+                         </tr>\
+                     ');
+                 
+             
+         }
+     
+                     fetch(url)
+                     .then(res=>res.json())
+                     .then((data)=>{
+                         data.forEach(function(el) {
+                        row(el);
+                         });
+                     })
+ //Post
+ 
+     let userInput = $('#username');
+      let EmailInput = $('#Email');
+                             
+     $("#sub-button").on("click", () => {
+ 
+                         $.ajax({
+                                         url: 'http://localhost:3000/users',
+                                         method: 'POST',
+                                         contentType: 'application/json',
+                                         data: JSON.stringify({
+                                         name: userInput.val(),
+                                         email: EmailInput.val()
+                                     }),
+                                         success: function(response) {
+                                             console.log(response);
+                             
+                                             userInput.val('');
+                                             EmailInput.val('');
+                                         
+                                         }
+                                     });
+ 
+                     })
+ //Delete
+                     $('table').on('click', '.delete-button', function() {
+                         var rowEl = $(this).closest('tr');
+                         var id = rowEl.find('.id').text();
+                         console.log(rowEl.html);
+ 
+                         $("#sub-button").on("click", () => {
+ 
+                         $.ajax({
+                             url: 'http://localhost:3000/users/'+ id,
+                             method: 'DELETE',
+                             contentType: 'application/json',
+                             success: function(response) {
+                                 console.log(response[0]);
+                                $('#sub-button').click();
+                             }
+                         });
+                         
+                     });
+                    
+                     });
+ 
+ //put
+ 
+ 
+ $('table').on('click', '.update-button', function() {
+     var rowEl = $(this).closest('tr');
+     var id = rowEl.find('.id').text();
+     var newTitle = rowEl.find('.name').val();
+     var newBody = rowEl.find('.email').val();
+     console.log(newTitle); 
+     console.log(newBody); 
+ $.ajax({
+                 url:   'http://localhost:3000/users/'+ id,
+                 method: 'PUT',
+                 contentType: 'application/json',
+                 data: JSON.stringify({ name: newTitle, email: newBody  }),
+                 success: function(response) {
+                    // loadDatap();
+                      $('#sub-button').click();
+                 }
+             });
+         });
+ 
+             });
+ 
